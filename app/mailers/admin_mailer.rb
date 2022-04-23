5dfa3700c2b91cb5cbebdbdbb5e9b123440e3f45
BelaBartok39 <jackthelion83@gmail.com>
2022-04-23 14:49:33 -0500

    deleted temp credits

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,11 @@
 class AdminMailer < ApplicationMailer
   default from: 'no-reply@strangetrails.net'
   default to: 'Nicky <nicky@strangetrails.net'
-end
 
-def new_user(user) {
-  mail(subject: "New User: #{user.email}")
-}
+
+  def new_user(user)
+    @user = user
+    mail(subject: "New User: #{user.email}")
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :send_notification
+
+  def send_notification
+    AdminMailer.new_user(self).deliver
+  
+  end
 end
