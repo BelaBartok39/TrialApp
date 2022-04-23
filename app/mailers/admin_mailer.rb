b784869ad356123c5696a297415a0ef870b08549
BelaBartok39 <jackthelion83@gmail.com>
2022-04-23 17:50:38 -0500

    Admin Mailer working, user emails working

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,11 +1,11 @@
-class AdminMailer < ApplicationMailer
-  default from: 'no-reply@strangetrails.net'
-  default to: 'Nicky <nicky@strangetrails.net'
+class AdminMailer < ActionMailer::Base
+  default from: MAIL_USER
+  default to: ADMIN_USER
 
 
   def new_user(user)
     @user = user
-    mail(subject: "New User: #{user.email}")
+    mail(subject: "New User")
 
   end
 end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,16 @@
-ActionMailer::Base.smtp_settings = {
-
-
-address: 'smtp.mailgun.org',
-port: 587,
-enable_starttls_auto: true,
-user_name: 'postmaster@sandboxcdf3174d6405473db2c22914264383d1.mailgun.org',
-password: 'c0803a43761589a253f59a5b8110dd50-02fa25a3-b8c3ab12',
-authentication: 'login'
-}
-
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.default charset: 'utf-8'
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address                => ADMIN_SMTP,
+  :port                   => ADMIN_SMTP_PORT,
+  :domain                 => ADMIN_DOMAIN,
+  :authentication         => :login,
+  :enable_starttls_auto   => true,
+  :openssl_verify_mode    => "none",
+  :user_name              => ADMIN_USER,
+  :password               => ADMIN_PASS,
+
+  # Added these. It works with either or both, at least for my configuration.
+  :ssl                    => true,
+  :tls                    => true,
+}
